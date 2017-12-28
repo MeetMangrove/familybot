e335e81e5e057bd98fcf94fea90c5138254c0ef5
Thomas Jeanneau <thomas@empathie.io>
2017-12-28 22:46:53 +0100

    update wording credentials gmail

--- main/api/gmail.js
+++ main/api/gmail.js
@@ -4,17 +4,17 @@ import GoogleAuth from 'google-auth-library'
 const {
   GMAIL_API_CLIENT_ID,
   GMAIL_API_CLIENT_SECRET,
-  GMAIL_API_ACCESS_TOKEN
+  GMAIL_API_CREDENTIALS
 } = process.env
 
-if (!GMAIL_API_CLIENT_ID || !GMAIL_API_CLIENT_SECRET || !GMAIL_API_ACCESS_TOKEN) {
-  console.log('Error: Specify GMAIL_API_CLIENT_ID, GMAIL_API_CLIENT_SECRET and GMAIL_API_ACCESS_TOKEN in a .env file')
+if (!GMAIL_API_CLIENT_ID || !GMAIL_API_CLIENT_SECRET || !GMAIL_API_CREDENTIALS) {
+  console.log('Error: Specify GMAIL_API_CLIENT_ID, GMAIL_API_CLIENT_SECRET and GMAIL_API_CREDENTIALS in a .env file')
   process.exit(1)
 }
 
 const auth = new GoogleAuth()
 const oauth2Client = new auth.OAuth2(GMAIL_API_CLIENT_ID, GMAIL_API_CLIENT_SECRET, 'postmessage')
-oauth2Client.credentials = JSON.parse(GMAIL_API_ACCESS_TOKEN)
+oauth2Client.credentials = JSON.parse(GMAIL_API_CREDENTIALS)
 
 export default google.gmail({
   version: 'v1',
