41a11dacf4bff768047dd20550427a49d10a686a
Simo Elalj <simo@elalj.com>
2020-03-24 16:49:26 +0100

    chanel name in env

--- main/rachid/crons/send_mood.js
+++ main/rachid/crons/send_mood.js
@@ -3,10 +3,14 @@ import cron from 'cron'
 import getMood from '../methods/convo/get_mood'
 import { bots } from '../config'
 
+require('dotenv').config()
+
+const channelName = process.env.MOOD_CHANNEL_NAME
+
 const sendMood = new cron.CronJob({
   cronTime: '00 00 19 * * *',
   onTick: async function () {
-    await getMood(bots[0], { channel: '#moods' })
+    await getMood(bots[0], { channel: `#${channelName}` })
   },
   start: false,
   timeZone: 'Europe/Paris'
